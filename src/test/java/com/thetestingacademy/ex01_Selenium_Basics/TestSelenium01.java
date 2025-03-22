3a30e9c691b159c4aec42299747d5da68c969992
Promode <pramoddutta_96@yahoo.com>
2025-03-22 10:05:00 +0530

    Part 1

--- src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
+++ src/test/java/com/thetestingacademy/ex01_Selenium_Basics/TestSelenium01.java
@@ -1,2 +1,17 @@
-package com.thetestingacademy.ex01_Selenium_Basics;public class TestSelenium01 {
+package com.thetestingacademy.ex01_Selenium_Basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+
+    @Test
+    public void test_VerifyVWOLogin(){
+        // Write the code which will execute the perform the ui interactions
+        FirefoxDriver driver = new FirefoxDriver();
+        driver.get("https://app.vwo.com");
+        System.out.println(driver.getTitle());
+    }
+
+
 }
